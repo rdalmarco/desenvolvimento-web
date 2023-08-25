01a4913b38ea47d7385da0294dcf8a759585f76e
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-08-25 20:15:03 -0300

    25082023

---
+++ erro.js
@@ -0,0 +1,19 @@
+
+//Criando exception
+const IllegalArgumentError = function(msg) {
+    this.name = 'IllegalArgumentError';
+    this.message = msg;
+};
+
+//Function com declaração para lançar a exception criada
+function soma (a, b) {
+    if(!a || !b) throw new IllegalArgumentError('erro');
+    return a + b;
+}
+
+//Capturando exception 
+try {
+    soma(2);
+} catch (err) {
+    document.querySelector('#erro').textContent = err.message;
+}
--- eventos.js
+++ eventos.js
@@ -58,21 +58,3 @@ const i = document.querySelector('#imagem');
 d.addEventListener('click', mostrarAlertaDiv);
 i.addEventListener('click', mostrarAlertaImg);
 
-//Criando exception
-const IllegalArgumentError = function(msg) {
-    this.name = 'IllegalArgumentError';
-    this.message = msg;
-};
-
-//Function com declaração para lançar a exception criada
-function soma (a, b) {
-    if(!a || !b) throw new IllegalArgumentError('erro');
-    return a + b;
-}
-
-//Capturando exception 
-try {
-    soma(2, 3);
-} catch (err) {
-    document.querySelector('#erro').textContent = err.message;
-}
